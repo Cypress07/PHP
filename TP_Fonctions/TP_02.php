3df74b76f12f492f8c54a6904bcd402814ec63bf
Cypress07 <58229011+Cypress07@users.noreply.github.com>
2020-02-17 17:32:08 +0100

    Update TP_02.php

--- TP_Fonctions/TP_02.php
+++ TP_Fonctions/TP_02.php
@@ -2,39 +2,31 @@
 
 $tableau1 = array();
   
-//Fonction pour échanger la valeur dans un tableau
-//Le tableau est passé par référence
 function echanger(&$tableau, $i, $j)
 {
-   $temporaire = $tableau[$i];
-   $tableau[$i] = $tableau[$j];
-   $tableau[$j] = $temporaire;
-}
-  
-//remplissage d’un tableau de 10 valeurs aléatoires entre 1 et 100
+   $temp= $tab[$i];
+   $tab[$i] = $tab[$j];
+   $tab[$j] = $temp ;
+
 for ($i=1;$i<=10;$i++) {
-    $tableau1[$i]=rand(1,100);
+    $tab1[$i]=rand(1,100);
 }
   
   
-$longueur=10;
-while($longueur>0)
+$long=10;
+while($long>0)
 {
-  // recherche de la plus grande valeur du tableau
-  $maximum = 1;
-  for($i=1; $i<=$longueur; $i++) {
-     if($tableau1[$i]>$tableau1[$maximum]) {
-        $maximum = $i;
+  $max = 1;
+  for($i=1; $i<=$long; $i++) {
+     if($tab1[$i]>$tab1[$max]) {
+        $max = $i;
     }
   }
-  //échange du maximum avec le dernier élément
-    echanger($tableau1, $maximum, $longueur);
+    echanger($tab1, $max, $long);
    
-  // Traitement du reste du tableau
-  $longueur--;
+  $long--;
 }
   
-//affichage du tableau
-for ($i=1;$i<=10;$i++) { //tableau de 1 à 10
-    echo $tableau1[$i].", ";
+for ($i=1;$i<=10;$i++) {
+    echo $tab1[$i].", ";
 }
