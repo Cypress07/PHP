419faff5f803bc7ef21865b400b88a6254b6bc05
MIKAEL OUZERA <m.ouzera@free.fr>
2020-02-18 09:56:28 +0100

    EXOS

--- TP_Fonctions/TP_01.php
+++ TP_Fonctions/TP_01.php
@@ -1,8 +1,6 @@
 <?php
-///Creer un tableau de 1 à 10 et afficher les valeurs séparées par un ;.
+
 $tab = array (1,2,3,4,5,6,7,8,9,10);
 
         echo implode (" ; ",$tab);
     
-    
-   
---
+++ TP_Fonctions/TP_02BoucleFor.php
@@ -0,0 +1,26 @@
+<?php
+define ('nb', 10);
+$tab = array();
+for ($i=1;$i<=nb;$i++) {
+    $tab[$i]=rand(1,100);
+    }
+  
+function echanger(&$tab, $i, $j)
+{
+   $tmp = $tab[$i];
+   $tab[$i] = $tab[$j];
+   $tab[$j] = $tmp;
+}  
+
+  for($nbr=nb; $nbr>1; $nbr--) {
+      $max = 1;
+    for ($i=2 ; $i<=$nbr; $i++){
+      if($tab[$i]>$tab[$max]) {
+        $max = $i;
+    }
+  }
+  echanger($tab, $max, $nbr);
+} 
+    echo implode (",", $tab);
+  
+    
